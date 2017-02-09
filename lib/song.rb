115b0d6fa8627c5d99f0abba324c05bb811d15d6
dison  ruan <disonruan@disons-MacBook-Air.local>
2017-02-08 19:29:21 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,59 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.bsearch{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    return self.find_by_name(name) if @@all.include?(name)
+    self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort{|a,b| a.name <=> b.name}
+  end
+
+  def self.new_from_filename(file_name)
+    artist_name = file_name.split(" - ")[0]
+    name = file_name[0...file_name.length-4].split(" - ")[1]
+    song = self.new
+    song.name = name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(file_name)
+    artist_name = file_name.split(" - ")[0]
+    name = file_name[0...file_name.length-4].split(" - ")[1]
+    song = self.new
+    song.name = name
+    song.artist_name = artist_name
+    @@all << song
+    song
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
 end
